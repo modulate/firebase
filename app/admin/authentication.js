dae95ce755e213b298819b87a17e80f9ff5b974e
Jared Hanson <jaredhanson@gmail.com>
2019-04-27 17:45:58 -0700

    Remove dead code.

--- app/admin/authentication.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function(app) {
-  return app.auth();
-};
-
-exports['@implements'] = 'http://schemas.modulate.io/js/opt/firebase/admin/Authentication';
-exports['@singleton'] = true;
-exports['@require'] = [ './app' ];
