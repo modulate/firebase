d4600ad44b4cbd71bca8d0e835af6169481d8e8e
Jared Hanson <jaredhanson@gmail.com>
2019-04-28 19:03:13 -0700

    Add tests.

--- test/package.test.js
+++ test/package.test.js
@@ -1,13 +1,26 @@
 /* global describe, it */
 
-var pkg = require('..');
 var expect = require('chai').expect;
 
 
-describe('firebase', function() {
+describe('@modulate/firebase', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  describe('package.json', function() {
+    var json = require('../package.json');
+    
+    it('should have assembly metadata', function() {
+      expect(json.assembly.namespace).to.equal('io.modulate/vnd/firebase');
+      
+      expect(json.assembly.components).to.have.length(4);
+      expect(json.assembly.components).to.include('authentication/directory');
+      expect(json.assembly.components).to.include('authentication/credentials/password');
+    });
+  });
+  
+  it('should throw if required', function() {
+    expect(function() {
+      var pkg = require('..');
+    }).to.throw(Error).with.property('code', 'MODULE_NOT_FOUND');
   });
   
 });
