cf32543a2a79d047518e23b75aed98eb2c3c90d9
Jared Hanson <jaredhanson@gmail.com>
2019-05-04 11:24:41 -0700

    Add credential support.

--- app/authentication/credentials/password.js
+++ app/authentication/credentials/password.js
@@ -1,4 +1,4 @@
-exports = module.exports = function() {
+exports = module.exports = function(creds) {
   var Client = require('../../../lib/auth/passwordclient');
   
   
@@ -6,8 +6,12 @@ exports = module.exports = function() {
   
   // TODO: add inferType from URL method
   
-  api.createConnection = function(options) {
-    return new Client(options.url);
+  api.createConnection = function(options, connectListener) {
+    var client = new Client(options.url);
+    client._creds = creds;
+    
+    client.connect(connectListener);
+    return client;
   }
   
   return api;
@@ -16,3 +20,6 @@ exports = module.exports = function() {
 exports['@singleton'] = true;
 exports['@implements'] = 'http://schemas.authnomicon.org/sd/IService';
 exports['@type'] = 'firebase-password-http';
+exports['@require'] = [
+  'http://i.bixbyjs.org/security/CredentialsStore'
+];
